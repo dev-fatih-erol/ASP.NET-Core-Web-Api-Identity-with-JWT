fed39073e9d2f560a38a20258a0c0dce94469347
dev-fatih-erol <dev.fatih.erol@gmail.com>
2020-05-07 21:58:06 +0300

    .

--- Users.Api/Models/RegisterDto.cs
+++ Users.Api/Models/RegisterDto.cs
@@ -14,9 +14,11 @@ namespace Users.Api.Models
         public string UserName { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
