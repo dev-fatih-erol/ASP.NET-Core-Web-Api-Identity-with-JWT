fe1871dd8c1f70044572c3aedc7b3ec27832ed2c
dev-fatih-erol <dev.fatih.erol@gmail.com>
2020-05-17 18:08:49 +0300

    .

--- Users.Api/Controllers/UserController.cs
+++ Users.Api/Controllers/UserController.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Users.Api.Data.Entities;
+using Users.Api.Infrastructure.Extensions;
 using Users.Api.Infrastructure.Helpers;
+using Users.Api.Models;
 
 namespace Users.Api.Controllers
 {
@@ -33,6 +36,26 @@ namespace Users.Api.Controllers
             }
 
             return Ok(_mapper.MapToUserDto(user));
+        }
+
+        [HttpPut]
+        [Authorize]
+        [Route("User/Password")]
+        public async Task<IActionResult> ChangePassword([FromBody]ChangePasswordDto request)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var result = await _userManager.ChangePasswordAsync(user, request.OldPassword, request.NewPassword);
+            if (result.Succeeded)
+            {
+                return Ok("Your password has been changed.");
+            }
+
+            return BadRequest(result.GetError());
         }
     }
 }
