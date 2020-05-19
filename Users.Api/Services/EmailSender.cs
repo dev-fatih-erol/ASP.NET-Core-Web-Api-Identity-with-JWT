154d405e2a142d6ed2b9684cb8ad67bc0e338eff
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-05-19 18:31:59 +0300

    .

--- Users.Api/Services/EmailSender.cs
+++ Users.Api/Services/EmailSender.cs
@@ -1,6 +1,4 @@
-﻿using System.Net;
-using System.Net.Mail;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Users.Api.Services
 {
@@ -8,27 +6,6 @@ namespace Users.Api.Services
     {
         public Task SendEmailAsync(string email, string subject, string body)
         {
-            SmtpClient sc = new SmtpClient
-            {
-                Port = 587,
-                Host = "smtp.gmail.com",
-                EnableSsl = true,
-                Credentials = new NetworkCredential("fatih.erol108@gmail.com", "Application108")
-            };
-
-            MailMessage mail = new MailMessage
-            {
-                From = new MailAddress("fatih.erol108@gmail.com", "Ekranda Görünecek İsim")
-            };
-
-            mail.To.Add(email);
-
-            mail.Subject = subject;
-            mail.IsBodyHtml = true;
-            mail.Body = body;
-
-            sc.Send(mail);
-
             return Task.CompletedTask;
         }
     }
